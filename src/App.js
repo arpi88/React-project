322f15b8a8d9f9d59ee20ac7d9b183e33aafa57a
arpi88 <arpinehakobyan58@gmail.com>
2022-08-07 20:19:22 -0700

    onex-homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+const Card = ()=>{
+  return(
+    <div className="Card-wrapper">
+    <div className="Card-wrapper-text">
+    <h1>
+    ԱՐՏԱՀԱՆՈՒՄ
+    </h1>
+    <p1>Ուղարկե՛ք Ձեր փաստաթղթերը, ծանրոցները և բեռները
+    ՀՀ-ից դեպի ԱՄՆ-ի և Ռուսաստանի ցանկացած հասցե  </p1>
+    </div>
+    <div className="Cards-section">
+    <div className="Card1">
+      <img src="https://onex.am/img/pages/export/export_usa.jpg"className="img1" alt="Card1" /> 
+      <p className="Card-text">Արտահանում ՀՀ-ից դեպի ԱՄՆ</p>
+    </div>
+    <div className="Card2">
+      <img src="https://onex.am/img/pages/export/export_rus.jpg"  className="img2"alt="Card2" />
+      <p className="Card-text">Արտահանում ՀՀ-ից դեպի Ռուսաստան</p>
+    </div>
+    <div className="Card3">
+      <img src="https://onex.am/img/docs.svg"className="img3"alt="Card3" />
+      <p className="Card-text3">Փաստաթղթերի առաքում ՀՀ-ից դեպի աշխարհի ցանկացած կետ</p>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    </div>
+    </div>
     </div>
   );
+}
+
+
+
+
+function App() {
+    return (
+    <div className="App"> 
+    <Card/>
+    </div>
+    );
 }
 
 export default App;
