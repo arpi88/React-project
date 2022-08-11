ab07e94cdc85342504cae44ede5a764e2acf478a
arpi88 <arpinehakobyan58@gmail.com>
2022-08-10 23:21:00 -0700

    Homework2 changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const Mainsection=()=>{
+  return(<div className="main-section">
+  <HeadingSection/>
+  <Wrapper/>
+  <Footertext/>
+  </div>
+  )
+}
+const HeadingSection=()=>{
+  return(
+    <div className="heading-text">
+      <h1>ԻՆՉՊԵՍ ՕԳՏՎԵԼ</h1>
+      <p>Ինչպե՞ս կատարել օնլայն գնումներ և ստանալ Հայաստանում արագ և ապահով</p>
+</div>
+  )
+}
+const Card=(props)=>{
+  const {imgUrl,label}=props;
+  return(
+  <div className="card">
+ 
+    <img src={imgUrl} className="img" alt="img"/>
+    <p className="card-text"> {label}</p>
+    </div>
+    )
+  
+}
+
+const Wrapper=()=>{
+  return(
+  <div className="card-wrapper">
+    <Card
+    imgUrl ={"https://onex.am/img/svg/step1.svg"}
+    label ={"Գրանցվե՛ք մեր կայքում և անվճար ստացե՛ք առաքման հասցեներ"}/>
+    < Card
+    imgUrl={"https://onex.am/img/svg/step2.svg"}
+    label ={"Օգտագործե՛ք Ձեզ տրամադրված առաքման հասցեները կայքերից գնումներ կատարելիս"}/>
+    <Card
+    imgUrl={"https://onex.am/img/svg/step3.svg"}
+    label={"Ստացե՛ք Ձեր գնումները ՀՀ բոլոր քաղաքներում և Արցախում"}/>
+    </div>
+  )}
+  const Footertext=()=>{
+    return(
+      <p>Կարող եք օգտվել նաև «Գնի՛ր իմ փոխարեն» ծառայությունից</p>
+    )
+  }
+  
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Mainsection/>
+    
     </div>
   );
 }
