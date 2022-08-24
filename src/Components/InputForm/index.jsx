650e319e0c9c4794386c9c48d7c0bc3b57f94aef
arpi88 <arpinehakobyan58@gmail.com>
2022-08-24 01:24:06 -0700

    lesson5

---
+++ src/Components/InputForm/index.jsx
@@ -0,0 +1,9 @@
+import "./style/css";
+export const InputForm = () => {
+  return (
+    <form className="input-form">
+      <input type="text"></input>
+      <button>Add</button>
+    </form>
+  );
+};
---
+++ src/Components/TextList/index.jsx
@@ -0,0 +1,10 @@
+import { NewList } from "./NewList";
+import { InputForm } from "./InputForm";
+export const TextList = () => {
+  return (
+    <div>
+      <InputForm />
+      <NewList />
+    </div>
+  );
+};
