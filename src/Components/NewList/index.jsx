9b986b483bba34c535e94eeacc030275849a59bf
arpi88 <arpinehakobyan58@gmail.com>
2022-08-28 19:51:59 -0700

    Delete

--- src/Components/NewList/index.jsx
+++ src/Components/NewList/index.jsx
@@ -1,12 +1,17 @@
 import "./style.css";
 export const NewList = ({ textData }) => {
+  const handleChange = (e) => {
+    e.input.value = "";
+  };
   return (
     <div className="list">
       {textData.map((text, index) => {
         return (
           <p className="list-item" key={index}>
             {text}
-            <button className="list-btn">Delete</button>
+            <button className="list-btn" onClick={handleChange}>
+              Delete
+            </button>
           </p>
         );
       })}
