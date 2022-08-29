a55f0bf9c46e3d52e18fbcd1f0a3c9cbfea508d6
AshotHeb <58462105+AshotHeb@users.noreply.github.com>
2022-08-29 07:24:02 +0400

    Fixed code after homework review

--- src/Components/NewList/index.jsx
+++ src/Components/NewList/index.jsx
@@ -1,15 +1,17 @@
 import "./style.css";
-export const NewList = ({ textData }) => {
-  const handleChange = (e) => {
-    e.input.value = "";
+
+export const NewList = ({ textData, setTextData }) => {
+  const handleDelete = (index) => {
+    setTextData(prev => prev.filter((text, idx) => idx !== index))
   };
+
   return (
     <div className="list">
       {textData.map((text, index) => {
         return (
           <p className="list-item" key={index}>
             {text}
-            <button className="list-btn" onClick={handleChange}>
+            <button className="list-btn" onClick={() => handleDelete(index)}>
               Delete
             </button>
           </p>
--- src/Components/TextList/index.jsx
+++ src/Components/TextList/index.jsx
@@ -3,10 +3,11 @@ import { NewList } from "../NewList";
 import { InputForm } from "../InputForm";
 export const TextList = () => {
   const [textData, setTextData] = useState([]);
+
   return (
     <div>
       <InputForm setTextData={setTextData} />
-      <NewList textData={textData} />
+      <NewList textData={textData} setTextData={setTextData}/>
     </div>
   );
 };
