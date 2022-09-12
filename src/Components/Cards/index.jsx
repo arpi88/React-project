387a89cf23683c08591fe8c093d7bd0a38010b93
arpi88 <arpinehakobyan58@gmail.com>
2022-09-11 18:22:38 -0700

    todo-changes

--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -1,16 +1,14 @@
 import "./style.css";
 import { Card, CardBody, CardTitle, CardText, Button } from "reactstrap";
 
-export const Cards = ({ todo: { title, description, id }, deleteData }) => {
+export const Cards = ({ todo: { title, description, id } }) => {
   return (
     <Card className="card-wrapper" style={{ width: "18rem" }}>
       <CardBody>
         <CardTitle tag="h5">{title}</CardTitle>
         <CardText>{description}</CardText>
         <div className="button-wrapper">
-          <Button className="delete-button" onClick={deleteData(id)}>
-            Delete
-          </Button>
+          <Button className="delete-button">Delete</Button>
           <Button className="edit-button">Edit</Button>
         </div>
       </CardBody>
